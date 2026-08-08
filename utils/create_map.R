c713b73220871527d45674244ef82045840dabad
quir1869 <quir1869@uw.edu>
2026-08-07 18:04:24 -0700

    Revise map function

--- utils/create_map.R
+++ utils/create_map.R
@@ -1,16 +1,10 @@
-library(rnaturalearth)
-library(rnaturalearthdata)
-library(sf)
-
 create_map <- function(data = data,
                        fill = "prop_missing",
                        country.col.name,
                        color.scale = TRUE) {
   
-  # Define Robinson projection
-  robinson_crs <- "+proj=robin +datum=WGS84"
+  proj_crs <- "+proj=natearth +datum=WGS84"   # Natural Earth projection
   
-  # Create ocean polygon
   ocean <- st_polygon(list(cbind(
     c(seq(-180, 179, length.out = 100), rep(180, 100), seq(179, -180, length.out = 100), rep(-180, 100)),
     c(rep(-90, 100), seq(-89, 89, length.out = 100), rep(90, 100), seq(89, -90, length.out = 100))
@@ -18,10 +12,8 @@ create_map <- function(data = data,
     st_sfc(crs = "WGS84") %>%
     st_as_sf()
   
-  # Transform ocean polygon
-  ocean_proj <- st_transform(ocean, crs = robinson_crs)
+  ocean_proj <- st_transform(ocean, crs = proj_crs)
   
-  # Load and patch world map
   world_map <- ne_countries(scale = "medium", returnclass = "sf") %>%
     mutate(iso_a3 = case_when(
       sovereignt == "Norway" ~ "NOR",
@@ -29,14 +21,22 @@ create_map <- function(data = data,
       TRUE ~ iso_a3
     )) %>%
     left_join(data, by = c("iso_a3" = country.col.name)) %>%
-    st_transform(crs = robinson_crs)
+    st_transform(crs = proj_crs) %>%
+    filter(sovereignt != "Antarctica")
   
-  # Plot
+  bbox <- st_bbox(ocean_proj)
+  pad <- 0.02 * max(bbox["xmax"] - bbox["xmin"], bbox["ymax"] - bbox["ymin"])
+  
   ggplot() +
-    geom_sf(data = ocean_proj, fill = "#8080ff80") +
+    geom_sf(data = ocean_proj, fill = "#e0e0e0") +
     geom_sf(data = world_map, aes(fill = !!sym(fill)), color = "grey30", linewidth = 0.05) +
-    {if (color.scale) scale_fill_viridis_c(option = "plasma") else scale_fill_manual(values = "gray80")} +
-    coord_sf(crs = robinson_crs, expand = FALSE, default = FALSE) +
+    {if (color.scale) scale_fill_distiller(palette = "OrRd", direction = 1, na.value = "#f2f2f2") else scale_fill_manual(values = "#f2f2f2")} +
+    coord_sf(
+      crs = proj_crs,
+      xlim = c(bbox["xmin"] - pad, bbox["xmax"] + pad),
+      ylim = c(bbox["ymin"] - pad, bbox["ymax"] + pad),
+      expand = FALSE
+    ) +
     theme_void() +
     theme(
       legend.position = "bottom",
